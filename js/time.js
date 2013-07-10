3f286b962d1d922cc0f0bf7462394619f6a78b45
Mostafa Rokooie <mostafa.rokooie@gmail.com>
2013-07-11 01:15:40 +0430

    Initial commit, a page with time remaining to Iftar ;)

---
+++ js/time.js
@@ -0,0 +1,28 @@
+var maghribSpan, times,
+    coordinates, timezone, dst;
+
+function calculateTimes() {
+    prayTimes.setMethod('Tehran');
+    times = prayTimes.getTimes(new Date(), coordinates, timezone, dst);
+}
+
+function update() {
+    var maghribTime = times.maghrib.split(':'),
+        maghrib = moment().hours(maghribTime[0]).minutes(maghribTime[1]).seconds(0);
+
+    maghribSpan.innerHTML = maghrib.fromNow();
+}
+
+function run() {
+    maghribSpan = document.getElementById('-app-maghrib-time');
+
+    coordinates = [35.6, 51.4];
+    timezone = 3.5;
+    dst = 1;
+
+    calculateTimes();
+    setInterval(calculateTimes, 1000 * 60);
+
+    update();
+    setInterval(update, 1000);
+}
