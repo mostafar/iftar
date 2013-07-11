dd3cd9812b358374af69779d60e538f4dc8e899a
Mostafa Rokooie <mostafa.rokooie@gmail.com>
2013-07-11 14:29:24 +0430

    Add language support, Persian and Arabic added.

--- js/time.js
+++ js/time.js
@@ -1,6 +1,21 @@
 var maghribSpan, times,
     coordinates, timezone, dst;
 
+var languages = {
+    en: {
+        label: 'English',
+        message: 'Time of Iftar'
+    },
+    fa: {
+        label: 'فارسی',
+        message: 'زمان افطار'
+    },
+    ar: {
+        label: 'العربية',
+        message: 'وقت الإفطار'
+    }
+};
+
 function calculateTimes() {
     prayTimes.setMethod('Tehran');
     times = prayTimes.getTimes(new Date(), coordinates, timezone, dst);
@@ -13,10 +28,26 @@ function update() {
     maghribSpan.innerHTML = maghrib.fromNow();
 }
 
-function run() {
-    var iframe = document.getElementById('-app-github-button');
-    iframe.src = iframe.attributes['data-src'].value;
+function changeLanguage(index) {
+    moment.lang(index);
+    document.getElementById('-app-time-label').innerHTML = languages[index].message;
+    update();
+}
 
+function loadLanguages() {
+    changeLanguage('en');
+
+    var langsContainer = document.getElementById('-app-langs-container');
+    for (var langIndex in languages) {
+        var lang = languages[langIndex];
+        var span = document.createElement('span');
+        span.innerHTML = lang.label;
+        span.addEventListener('click', changeLanguage.bind({}, langIndex));
+        langsContainer.appendChild(span);
+    }
+}
+
+function run() {
     maghribSpan = document.getElementById('-app-maghrib-time');
 
     coordinates = [35.6, 51.4];
@@ -28,4 +59,9 @@ function run() {
 
     update();
     setInterval(update, 1000);
+
+    loadLanguages();
+
+    var iframe = document.getElementById('-app-github-button');
+    iframe.src = iframe.attributes['data-src'].value;
 }
